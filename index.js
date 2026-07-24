013c2d4418c4162df661d77884e304d0baaf391c
Boris Boulogne <boulogneboris@gmail.com>
2026-07-24 17:16:26 +0200

    First version of rotation done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', () => {
-    initForwardClick()
+    initRotationTriggers()
     initExpandClick()
 
 })
 
-function initForwardClick() {
-    document.querySelectorAll('.forward').forEach(clickable => {
-        clickable.addEventListener('click', (e) => {
-            // console.log(e.target.closest('.video-container').querySelector('h2').innerHTML);
 
-            console.log('Forward');
-        })
+function initRotationTriggers() {
+    document.querySelectorAll('.clockwise').forEach(cwTrigger => {
+        cwTrigger.addEventListener('click', moveElementsClockwise)
+    })
+    document.querySelectorAll('.anti-clockwise').forEach(acwTrigger => {
+        acwTrigger.addEventListener('click', moveElementsAntiClockwise)
     })
 }
 
@@ -25,3 +25,30 @@ function initExpandClick() {
     })
 }
 
+function moveElementsAntiClockwise(e) {
+    // console.log('anti');
+    if (document.querySelector('[data-pos="3"]')) {
+        document.querySelector('[data-pos="3"]').setAttribute('data-pos', 4)
+        document.querySelector('[data-pos="2"]').setAttribute('data-pos', 6)
+        document.querySelector('[data-pos="1"]').setAttribute('data-pos', 5)
+    }
+    else {
+        document.querySelector('[data-pos="6"]').setAttribute('data-pos', 1)
+        document.querySelector('[data-pos="5"]').setAttribute('data-pos', 3)
+        document.querySelector('[data-pos="4"]').setAttribute('data-pos', 2)
+    }
+}
+function moveElementsClockwise() {
+    // console.log('clockwise');
+    if (document.querySelector('[data-pos="2"]')) {
+        document.querySelector('[data-pos="2"]').setAttribute('data-pos', 4)
+        document.querySelector('[data-pos="3"]').setAttribute('data-pos', 5)
+        document.querySelector('[data-pos="1"]').setAttribute('data-pos', 6)
+    }
+    else {
+        document.querySelector('[data-pos="5"]').setAttribute('data-pos', 1)
+        document.querySelector('[data-pos="6"]').setAttribute('data-pos', 2)
+        document.querySelector('[data-pos="4"]').setAttribute('data-pos', 3)
+    }
+
+}
