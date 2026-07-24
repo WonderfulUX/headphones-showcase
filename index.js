b9fe18420988af981a6ec47a6793c3e34492a873
Boris Boulogne <boulogneboris@gmail.com>
2026-07-24 23:48:22 +0200

    Rotation Ok clockwise and anticlockwise

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ function initExpandClick() {
     document.querySelectorAll('.expand').forEach(expandable => {
         expandable.addEventListener('click', (e) => {
             console.log('expand');
-
-            // console.log(e.target.closest('.video-container').querySelector('h2').innerHTML);
-
         })
     })
 }
@@ -28,27 +25,47 @@ function initExpandClick() {
 function moveElementsAntiClockwise(e) {
     // console.log('anti');
     if (document.querySelector('[data-pos="3"]')) {
+        document.querySelector('[data-pos="3"]').classList.add('ctn-antiClockwise')
+        document.querySelector('[data-pos="2"]').classList.add('ctn-antiClockwise')
+        document.querySelector('[data-pos="1"]').classList.add('ctn-antiClockwise')
         document.querySelector('[data-pos="3"]').setAttribute('data-pos', 4)
         document.querySelector('[data-pos="2"]').setAttribute('data-pos', 6)
         document.querySelector('[data-pos="1"]').setAttribute('data-pos', 5)
     }
     else {
+        document.querySelector('[data-pos="6"]').classList.add('ctn-antiClockwise')
+        document.querySelector('[data-pos="5"]').classList.add('ctn-antiClockwise')
+        document.querySelector('[data-pos="4"]').classList.add('ctn-antiClockwise')
         document.querySelector('[data-pos="6"]').setAttribute('data-pos', 1)
         document.querySelector('[data-pos="5"]').setAttribute('data-pos', 3)
         document.querySelector('[data-pos="4"]').setAttribute('data-pos', 2)
     }
+    document.querySelectorAll('.ctn-antiClockwise').forEach(element => {
+        element.addEventListener('transitionend', () => {
+            element.classList.remove('ctn-antiClockwise')
+        })
+    })
 }
 function moveElementsClockwise() {
-    // console.log('clockwise');
     if (document.querySelector('[data-pos="2"]')) {
+        document.querySelector('[data-pos="2"]').classList.add('ctn-clockwise')
+        document.querySelector('[data-pos="3"]').classList.add('ctn-clockwise')
+        document.querySelector('[data-pos="1"]').classList.add('ctn-clockwise')
         document.querySelector('[data-pos="2"]').setAttribute('data-pos', 4)
         document.querySelector('[data-pos="3"]').setAttribute('data-pos', 5)
         document.querySelector('[data-pos="1"]').setAttribute('data-pos', 6)
     }
     else {
+        document.querySelector('[data-pos="5"]').classList.add('ctn-clockwise')
+        document.querySelector('[data-pos="6"]').classList.add('ctn-clockwise')
+        document.querySelector('[data-pos="4"]').classList.add('ctn-clockwise')
         document.querySelector('[data-pos="5"]').setAttribute('data-pos', 1)
         document.querySelector('[data-pos="6"]').setAttribute('data-pos', 2)
         document.querySelector('[data-pos="4"]').setAttribute('data-pos', 3)
     }
-
+    document.querySelectorAll('.ctn-clockwise').forEach(element => {
+        element.addEventListener('transitionend', () => {
+            element.classList.remove('ctn-clockwise')
+        })
+    })
 }
